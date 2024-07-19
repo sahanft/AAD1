ff6326349da3905e58962607242d6bd899cbf744
sahanft <udakarafernando2001@gmail.com>
2024-07-19 08:49:46 +0530

    1st commit

---
+++ src/main/java/lk/ijse/Serialization.java
@@ -0,0 +1,19 @@
+package lk.ijse;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+public class Serialization {
+    public static void main(String[] args) {
+       Student student = new Student("Shanilka","Sahan");
+
+        System.out.println("Sahan");
+
+        try (FileOutputStream fileOut = new FileOutputStream("student.json");
+             ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+            out.writeObject(student);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
